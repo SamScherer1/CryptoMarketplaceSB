fddf9e45594c963e5579be671a839f194dc7a6dc
SamScherer1 <samscherer33@gmail.com>
2023-01-14 20:05:55 -0500

    fix tests, but see if you wanna commit the change in xcschememanagement.plist

--- CryptoMarketplaceSBTests/CryptoMarketplaceSBTests.swift
+++ CryptoMarketplaceSBTests/CryptoMarketplaceSBTests.swift
@@ -12,43 +12,20 @@ final class CryptoMarketplaceSBTests: XCTestCase {
     
     var app: XCUIApplication!
 
-
-
-//    func testLaunchPerformance() {
-//        // Measure the launch time
-//        let start = Date()
-//        app.launch()
-//        let launchTime = Date().timeIntervalSince(start)
-//        print("App launched in \(launchTime) seconds")
-//    }
-
     override func setUpWithError() throws {
-//        override func setUp() {
-            super.setUp()
-            continueAfterFailure = false
-            app = XCUIApplication()
-        //}
+        super.setUp()
+        continueAfterFailure = false
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
-    func testLaunchPerformance() {
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                app.launch()
-            }
-        }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
-    func testPerformanceExample() throws {
+    func testFetchCoinsTime() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
